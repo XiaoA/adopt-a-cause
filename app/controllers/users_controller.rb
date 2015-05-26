6245e635bc49f07b14a43a3b351f61f4eec28d36
Andrew Buckingham <abuckingham99@gmail.com>
2015-05-25 21:37:13 -0500

    Add show/new/create methods in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
+  def show
+  end
+
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session:[:user_id] = @user.id
+      flash[:notice] = "Congratulations. You've created a new account."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 end

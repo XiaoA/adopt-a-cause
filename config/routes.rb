294f4a8a52c19166f952dc84a6f54a62ed22db05
XiaoA <abuckingham99@gmail.com>
2015-06-23 21:45:45 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-#  post '/volunteer', to: 'projects#volunteer'
-#  post '/projects/:id/volunteer', to: 'projects#volunteer'
-  put '/users/:id/projects', to: 'users#volunteer'
   get '/users/:id/projects', to: 'users#show'
-  post '/project_users/new', to: 'project_users#create'
+  post '/project_users', to: 'project_users#create'
+  post 'project_users/new', to: 'project_users#create'
+
 
   resources :users
   resources :projects
   resources :project_users
+
   
 end
